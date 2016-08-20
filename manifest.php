c9ff23f8e1a0a15414e016e04b4d9fe3f952e93b
Leon Nikitin <nlv@lab321.ru>
2016-08-20 18:19:51 +0600

    Добавлены модули проекты и проектные задачи

--- manifest.php
+++ manifest.php
@@ -6,9 +6,9 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin@pravmail.ru',
     'description' => 'Формы для semlot.',
     'is_uninstallable' => true,
-    'published_date' => '2016-08-18',
+    'published_date' => '2016-08-20',
     'type' => 'module',
-    'version' => '1.1.0',
+    'version' => '1.2.0',
 );
 $installdefs = array(
     'id' => 'semlot_forms',
